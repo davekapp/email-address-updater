1f7d57268cd585c16c1288b1d22aa2aff93a8087
David Kapp <davidkapp@coshx.com>
2012-02-02 21:24:07 -0700

    Initial Commit

---
+++ AddressEditorTests/AddressEditorTests.h
@@ -0,0 +1,13 @@
+//
+//  AddressEditorTests.h
+//  AddressEditorTests
+//
+//  Created by David Kapp on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AddressEditorTests : SenTestCase
+
+@end
---
+++ AddressEditorTests/AddressEditorTests.m
@@ -0,0 +1,32 @@
+//
+//  AddressEditorTests.m
+//  AddressEditorTests
+//
+//  Created by David Kapp on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AddressEditorTests.h"
+
+@implementation AddressEditorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AddressEditorTests");
+}
+
+@end
